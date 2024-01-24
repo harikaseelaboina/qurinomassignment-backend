2f9282c812f0289971b4ab8ce3f51b4cc6559620
Harika Seelaboina <54906795+harikaseelaboina@users.noreply.github.com>
2024-01-24 16:00:57 +0530

    Update Merchantapi.js

--- Routes/Merchantapi.js
+++ Routes/Merchantapi.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Merchant = require('../Schemas/Merchantschema'); 
+const cors = require('cors');
 
 const app = express();
 
