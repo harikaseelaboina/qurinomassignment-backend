04cb4f3a04d84d1119e8158dd235b8582cd1c32b
Harika Seelaboina <54906795+harikaseelaboina@users.noreply.github.com>
2024-01-24 15:56:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-
+app.use(cors());
 app.use(express.json());
 
 mongoose.connect('mongodb+srv://harika:harika123@cluster0.n2lfsjh.mongodb.net/?retryWrites=true&w=majority')
