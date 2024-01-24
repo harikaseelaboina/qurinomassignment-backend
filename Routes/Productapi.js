e6b1138395fd54cf7b7de13a83cbdea66fa10493
Harika Seelaboina <54906795+harikaseelaboina@users.noreply.github.com>
2024-01-24 16:01:16 +0530

    Update Productapi.js

--- Routes/Productapi.js
+++ Routes/Productapi.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Product = require('../Schemas/Productschema'); 
+const cors = require('cors');
 
 const app = express();
 app.use(cors());
