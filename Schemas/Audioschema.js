03787c681674088c5695342488200c5d2bdd8b7c
Harika Seelaboina <54906795+harikaseelaboina@users.noreply.github.com>
2024-03-04 10:21:24 +0530

    Update Audioschema.js

--- Schemas/Audioschema.js
+++ Schemas/Audioschema.js
@@ -1,1 +1,16 @@
+const mongoose = require('mongoose');
 
+
+const audioSchema = new mongoose.Schema({
+   agent: String,
+  surveyId: String,
+  category: String,
+  questionNumber: Number,
+  audioPath: String,
+  contentType: String,
+});
+
+
+const Audio = mongoose.model('Audio', audioSchema);
+
+module.exports = Audio;
